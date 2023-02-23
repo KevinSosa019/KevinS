e3564feb6a884e3bf638955e0df09f1886f78811
KevinSosa <kevinsosag25@gmail.com>
2023-02-22 18:21:26 -0600

    Mongodb

---
+++ app.py
@@ -0,0 +1,10 @@
+from classes import Estudiante
+from dotenv import load_dotenv
+
+def main():
+    estudiante = Estudiante("Uayeb 3","Caballero","31487539")
+    estudiante.save()
+    
+if __name__ == "__main__":
+    load_dotenv()
+    main()
